07d4ee6c8caa2feba287a003b320dc130da5d49d
MaxxAferist <94622572+MaxxAferist@users.noreply.github.com>
2024-03-21 20:53:26 +0300

    Контроллеры, нормальная сетка, можно ставить точки

--- src/init.js
+++ src/init.js
@@ -9,17 +9,30 @@ const init = () => {
 
 	const scene = new THREE.Scene();
 	const canvas = document.querySelector('.canvas');
-	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-	scene.add(camera);
+	const perspectiveCamera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+	scene.add(perspectiveCamera);
 
-	const controls = new OrbitControls(camera, canvas);
-	controls.enableDamping = true;
+	let height_camera = 32;
+	let width_camera = height_camera * (sizes.width / sizes.height);
+	const orthographicCamera = new THREE.OrthographicCamera(
+		-width_camera / 2, width_camera / 2,
+		height_camera / 2, -height_camera / 2,
+		0.1, 100);
+	scene.add(orthographicCamera);
+
+	const controls_persp = new OrbitControls(perspectiveCamera, canvas);
+	const controls_orth = new OrbitControls(orthographicCamera, canvas);
+	controls_persp.enableDamping = true;
+	controls_orth.enableDamping = true;
+
+	const raycaster = new THREE.Raycaster();
+	raycaster.params.Line.threshold = 3;
 
 	const renderer = new THREE.WebGLRenderer({ canvas });
 	renderer.setSize(sizes.width, sizes.height);
-	renderer.render(scene, camera);
+	renderer.render(scene, perspectiveCamera);
 
-	return { sizes, scene, canvas, camera, renderer, controls };
+	return { sizes, scene, canvas, perspectiveCamera, orthographicCamera, renderer, controls_persp, controls_orth, raycaster};
 };
 
 export default init;
--- src/script.js
+++ src/script.js
@@ -1,16 +1,72 @@
 import * as THREE from 'three';
-
+import * as dat from 'lil-gui';
 import init from './init';
 
 import './style.css';
+import { LatheGeometry } from 'three';
 
-const { sizes, camera, scene, canvas, controls, renderer } = init();
+const { sizes, scene, canvas, perspectiveCamera, orthographicCamera, renderer, controls_persp, controls_orth, raycaster } = init();
 
 const axisHelper = new THREE.AxesHelper(25);
 scene.add(axisHelper);
 
-camera.position.z = 30;
-camera.position.y = 5;
+const pointer = new THREE.Vector2();
+
+const cursor_point_geometry = new THREE.SphereGeometry(0.15);
+const cursor_material = new THREE.MeshStandardMaterial({
+	color: 0x999999,
+	visible: false,
+});
+const cursor_point = new THREE.Mesh(cursor_point_geometry, cursor_material);
+scene.add(cursor_point);
+
+const group_measuring_points = new THREE.Group();
+scene.add(group_measuring_points);
+
+let activeCamera = perspectiveCamera;
+let activeControls = controls_persp;
+let magnetizm = false;
+
+const config = {
+	'activeCamera': 'перспективная',
+	'camers': ['перспективная', 'вид сверху'],
+	'magnetizm': false,
+}
+
+const dict_camera = {
+	'перспективная': {
+		camera: perspectiveCamera,
+		position: new THREE.Vector3(0, 30, 0),
+		rotation: new THREE.Vector3(0, 0, 0),
+	},
+	'вид сверху': {
+		camera: orthographicCamera,
+		position: new THREE.Vector3(0, 30, 0),
+		rotation: new THREE.Vector3(0, 0, 0),
+	},
+}
+
+const dict_controls = {
+	'перспективная': {
+		controls: controls_persp,
+		enabled: true,
+	},
+	'вид сверху': {
+		controls: controls_orth,
+		enabled: false,
+	},
+}
+
+activeCamera.position.z = 0;
+activeCamera.position.y = 30;
+controls_persp.enabled = true;
+controls_orth.enabled = false;
+
+ /** DAT.GUI */
+const gui = new dat.GUI();
+const cameraController = gui.add(config, 'activeCamera', config['camers']).name('Камера');
+cameraController.onChange(changeCamera);
+const magnetController = gui.add(config, 'magnetizm').name('Магнитный курсор');
 
 /** Making PLANE */
 const plane_geometry = new THREE.PlaneGeometry(43, 30);
@@ -22,15 +78,28 @@ const plane_material = new THREE.MeshStandardMaterial({
 const plane = new THREE.Mesh(plane_geometry, plane_material);
 plane.rotation.x = -Math.PI * 0.5;
 plane.receiveShadow = true;
-scene.add(plane);
+// scene.add(plane);
+
+/** Making TABLE */
+const table_geometry = new THREE.BoxGeometry(43, 30, 1);
+const table_material = new THREE.MeshStandardMaterial({
+	color: 0xff0033,
+	metalness: 0,
+	roughness: 0.5,
+});
+const table = new THREE.Mesh(table_geometry, table_material);
+table.rotation.x = -Math.PI * 0.5;
+table.position.y -= table.geometry.parameters.depth / 2;
+table.receiveShadow = true;
+scene.add(table);
 
 /** Making LIGHT */
-const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.5);
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
 hemisphereLight.position.set(0, 50, 0);
 scene.add(hemisphereLight);
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-directionalLight.position.set( 88, 12, 8 );
+directionalLight.position.set( 20, 12, 8 );
 directionalLight.castShadow = true;
 directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
 scene.add(directionalLight);
@@ -39,93 +108,80 @@ scene.add(directionalLight);
 const electrod_geometry = new THREE.CylinderGeometry(1.5, 1.5, 20);
 const electrod_material = new THREE.MeshStandardMaterial({
 	color: 0x999999,
+	metalness: 0,
+	roughness: 0.5,
 })
 
 const electrod1 = new THREE.Mesh(electrod_geometry, electrod_material);
 electrod1.position.x = 9.5 + (plane.position.x - plane.geometry.parameters.width / 2);
 electrod1.position.z = -15 + (plane.position.z + plane.geometry.parameters.height / 2);
+electrod1.receiveShadow = true;
 scene.add(electrod1);
 
 const electrod2 = new THREE.Mesh(electrod_geometry, electrod_material);
 electrod2.position.x = 33.5 + (plane.position.x - plane.geometry.parameters.width / 2);
 electrod2.position.z = -15 + (plane.position.z + plane.geometry.parameters.height / 2);
+electrod2.receiveShadow = true;
 scene.add(electrod2);
 
-/** Making SETKA */
-const checkMesh_material = new THREE.LineBasicMaterial({
-	color: 0x00ff00,
-});
 
-const points = [];
+/** Making SETKA */
+const checkMesh = new THREE.Group();
+const checkMesh_material = new THREE.MeshStandardMaterial({
+	color: 0x66ff66,
+});
 
 const checkMesh_height = plane.geometry.parameters.height;
 const checkMesh_width = plane.geometry.parameters.width;
-for (let i = 0; i < checkMesh_width + 1; i++) {
-	const point1 = new THREE.Vector3(0, 0, 0);
-	point1.x = i + (plane.position.x - plane.geometry.parameters.width / 2);
-	point1.y = 0;
-	point1.z = (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point1);
-
-	const point2 = new THREE.Vector3(0, 0, 0);
-	point2.x = i + (plane.position.x - plane.geometry.parameters.width / 2);
-	point2.y = 0;
-	point2.z = -checkMesh_height + (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point2);
-
-	const point3 = new THREE.Vector3(0, 0, 0);
-	point3.x = i + (plane.position.x - plane.geometry.parameters.width / 2);
-	point3.y = 0;
-	point3.z = (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point3);
+for (let i = 1; i < checkMesh_width; i++) {
+	const geometry = new THREE.BoxGeometry(0.05, 0.05, checkMesh_height);
+	const line = new THREE.Mesh(geometry, checkMesh_material);
+	line.position.x = i + (plane.position.x - plane.geometry.parameters.width / 2);
+	checkMesh.add(line);
 }
 
-for (let i = 0; i < checkMesh_height + 1; i++) {
-	const point1 = new THREE.Vector3(0, 0, 0);
-	point1.x = (plane.position.x - plane.geometry.parameters.width / 2);
-	point1.y = 0;
-	point1.z = -i + (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point1);
-
-	const point2 = new THREE.Vector3(0, 0, 0);
-	point2.x = checkMesh_width + (plane.position.x - plane.geometry.parameters.width / 2);
-	point2.y = 0;
-	point2.z = -i + (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point2);
-
-	const point3 = new THREE.Vector3(0, 0, 0);
-	point3.x = (plane.position.x - plane.geometry.parameters.width / 2);
-	point3.y = 0;
-	point3.z = -i + (plane.position.z + plane.geometry.parameters.height / 2);
-	points.push(point3);
+for (let i = 1; i < checkMesh_height; i++) {
+	const geometry = new THREE.BoxGeometry(checkMesh_width, 0.05, 0.05);
+	const line = new THREE.Mesh(geometry, checkMesh_material);
+	line.position.z = i + (plane.position.z - plane.geometry.parameters.height / 2);
+	checkMesh.add(line);
 }
 
-const checkMesh_geometry = new THREE.BufferGeometry().setFromPoints(points);
-
-const checkMesh = new THREE.Line(checkMesh_geometry, checkMesh_material);
 scene.add(checkMesh);
 
 const tick = () => {
-	controls.update();
-	renderer.render(scene, camera);
+	activeControls.update();
+	renderer.render(scene, activeCamera);
 	window.requestAnimationFrame(tick);
 };
 tick();
 
 /** Базовые обпаботчики событий длы поддержки ресайза */
-window.addEventListener('resize', () => {
-	// Обновляем размеры
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
+window.addEventListener('resize', updateSizeAfterResize);
 
-	// Обновляем соотношение сторон камеры
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+document.addEventListener('pointermove', (event) => {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-	// Обновляем renderer
-	renderer.setSize(sizes.width, sizes.height);
-	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-	renderer.render(scene, camera);
+	if (config.activeCamera === 'вид сверху') {
+		raycaster.setFromCamera(pointer, activeCamera);
+		const intersects = raycaster.intersectObjects([table, checkMesh]);
+
+		if ( intersects.length > 0 ) {
+			if (config.magnetizm){
+				cursor_point.position.x = Math.round(intersects[0].point.x * 2) / 2;
+				cursor_point.position.z = Math.round(intersects[0].point.z * 2) / 2;
+			} else {
+				cursor_point.position.x = intersects[0].point.x;
+				cursor_point.position.z = intersects[0].point.z;
+			}
+			cursor_point.material.visible = true;
+		} else {
+			cursor_point.material.visible = false;
+		}
+	} else {
+		cursor_point.material.visible = false;
+	}
 });
 
 window.addEventListener('dblclick', () => {
@@ -135,3 +191,60 @@ window.addEventListener('dblclick', () => {
 		document.exitFullscreen();
 	}
 });
+
+window.addEventListener('click', (event) => {
+	if (config.activeCamera === 'вид сверху' && cursor_point.material.visible) {
+		const geometry_measuring_point = new THREE.SphereGeometry(cursor_point.geometry.parameters.radius);
+		const material_measuring_point = new THREE.MeshBasicMaterial({
+			color: 0xffcc33,
+		});
+		const measuring_point = new THREE.Mesh(geometry_measuring_point, material_measuring_point);
+		group_measuring_points.add(measuring_point);
+
+		measuring_point.position.x = cursor_point.position.x;
+		measuring_point.position.z = cursor_point.position.z;
+	}
+});
+
+function changeCamera() {
+	const value = config.activeCamera;
+	activeCamera = dict_camera[value].camera;
+	activeCamera.position.set(0, 30, 0);
+	activeCamera.rotation.set(0, 0, 0);
+	updateSizeAfterResize();
+	changeControls();
+};
+
+function changeControls() {
+	const value = config.activeCamera;
+	activeControls = dict_controls[value].controls;
+	activeControls.enabled = dict_controls[value].enabled;
+	for (let i = 0; i < config.camers.length; i++) {
+		if (config.camers[i] !== value){
+			const controls = dict_controls[config.camers[i]].controls;
+			controls.enabled = false;
+		}
+	}
+};
+
+function updateSizeAfterResize() {
+	// Обновляем размеры
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	let height_camera = 32;
+	let width_camera = height_camera * (sizes.width / sizes.height);
+	orthographicCamera.left = -width_camera / 2;
+	orthographicCamera.right = width_camera / 2;
+	orthographicCamera.bottom = -height_camera / 2;
+	orthographicCamera.top = height_camera / 2;
+
+	// Обновляем соотношение сторон камеры
+	activeCamera.aspect = sizes.width / sizes.height;
+	activeCamera.updateProjectionMatrix();
+
+	// Обновляем renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	renderer.render(scene, activeCamera);
+};
